e98259ba5fbbc026d2cea6942eff733b97a47659
iCarlosPro <carlosjavier.perez@alu.uhu.es>
2018-03-21 19:42:35 +0100

    impresión menú

--- P1ED1/main.cpp
+++ P1ED1/main.cpp
@@ -8,8 +8,21 @@ using namespace std;
 
 int main()
 {
+int numatletas=0;
+int menuop;
+do{
+system("cls");
 cout<< "\n Practica 1 de ED1        Carlos Javier Perez Sanchez \n";
 
+cout<<"\n Atletas: "<<numatletas<<" \n\n\n";
+
+cout<<"\t 1. Consulta de inscripciones \n\t 2. Inscripcion a la maraton \n\t 3. Modificar una inscripcion \n\t 4. Eliminar una inscripcion \n\t 5. Mostrar clasificacion \n\t 6. Salir \n\n";
+
+cout<<" Indique la opcion deseada: ";
+cin>>menuop;
+cin.ignore(1,'\n');
+
+} while(menuop!=6);
 system("pause");
     return 0;
 }
